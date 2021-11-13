4cddecc52438f691c4ea903caedca776a52c074b
Beccadyn <sarpongrebecca101@gmail.com>
2021-11-13 00:32:12 -0800

    Updated productdetails screen

--- Screens/productdetail.js
+++ Screens/productdetail.js
@@ -8,7 +8,8 @@ const products = {
         price: 500,
         size: "small, medium, large",
         Colors: 'black, navy blue, purple',
-        image:''
+        image:'office.jpg'
+        description: 'This office chair provides you with comfort. You can sit all day and be rest assured you be just great.'
     },
 
     women_shoes: {
@@ -17,7 +18,8 @@ const products = {
         price: 30,
         size: '36,37,38,40,41,42',
         Colors: 'black, browm, wine, army green',
-        image:''
+        image:'peach shoe.png'
+        description: 'What is better than going out whilw feeling so free? This shoe gives you that homely feeling. It comes in all sizes so be sure to try it out.'
     },
 
     hoodies: {
@@ -26,7 +28,8 @@ const products = {
         price: 50,
         size: 'small, medium, large',
         Colors: 'orange, white, blue, black, purple, coffee brown',
-        image:''
+        image:'crop_top_hoodie.png'
+        description:'This product provides you with warmth in the cold season. It is a go to option if you do not feel like dressing up sophisticated. It also gives you a classy look.'
     },
 
     men_shoes: {
